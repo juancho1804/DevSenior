ef570aba4c1cb55a380b79382db29f7f1ea6204c
Juan Manuel Ceron <138546864+juancho1804@users.noreply.github.com>
2026-03-10 19:58:24 -0500

    Aplicacion Estudiantes

---
+++ module-01/class-07/AplicacionEstudiantes.java
@@ -0,0 +1,10 @@
+public class AplicacionEstudiantes {
+    public static void main(String[] args) {
+
+    }
+
+    public static void imprimirMenu() {
+        System.out.println(
+                "--- Sistema de Registro de Estudiantes ---\n1. Registrar datos de un estudiante\n2. Mostrar datos del estudiante actual\n3. Calcular promedio de notas del estudiante actual\n0. Salir\nIngrese su opción:");
+    }
+}
